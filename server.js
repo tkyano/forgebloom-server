bf256cbadfee4c030cd8f940f0e61b6c5bbd2296
Timothy Kyano <tdkyano05@gmail.com>
2025-11-09 18:14:01 -0500

    test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
